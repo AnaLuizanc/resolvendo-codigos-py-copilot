e93fc47b618e7fb3a0248d9984b453786014899f
Ana Luiza <analuizanc13@gmail.com>
2024-10-19 00:57:03 +0000

    math operations implemented

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,10 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+number1 = float(input("Enter float: "))
+number2 = float(input("Enter float: "))
+
+print("Sum: ", number1+number2)
+print("Subtraction: ", number1-number2)
+print("Multiply: ", number1*number2)
+if number2 != 0:
+    print("Division: ", number1/number2)
