76aa43bf5286fb7401f59415369ee1974c67206d
Ana Luiza <analuizanc13@gmail.com>
2024-10-19 00:42:01 +0000

    repeated text implementation added

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,6 @@
 # Vamos solicitar como entrada uma string e um inteiro e depois vamos retornar a string repetida o número de vezes informado.
+
+string = input("Enter string: ")
+number = int(input("Enter integer: "))
+
+print((string + " ") * number)
