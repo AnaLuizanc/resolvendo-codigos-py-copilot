3b0e567b9f730e73ad0291806ef071f0de5ae1e2
Ana Luiza <analuizanc13@gmail.com>
2024-10-19 00:28:02 +0000

    concatenation code added

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,7 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+firstData = input("Enter first data: ");
+secondData = input("Enter second data: ");
+concated = firstData + " " + secondData
+
+print(concated)
